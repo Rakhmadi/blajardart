8cf4d1ae428bc2f89d52fb92ab5a129d2ad9032c
Rakhamdi <rakhmadiwalker@gmail.com>
2019-10-11 14:51:51 +0700

    s

---
+++ Arithmetic_Operators.dart
@@ -0,0 +1,14 @@
+void main(){
+  int x =12;
+  int y =3;
+  int a =  x + y; //penambahan
+  int b =  x - y; //pengurangan
+  int c =  x * y; //perkalian
+  double d =  x / y; //pembagian
+  int e = x ~/ y; //pembagian output bilanagn bulat
+  int f =x % y; //sisa hasil bagi
+  int g =x++; //increment penambahan +1
+  int h =x--;  //decrement pengurangan -1
+  var list =[  a,b,c,d,e,f,g,h];
+  print(list);
+}

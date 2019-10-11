72bc96fead87dfac84095d923d308acfa3153820
Rakhamdi <rakhmadiwalker@gmail.com>
2019-10-11 08:43:21 +0700

    first_commit

---
+++ variable.dart
@@ -0,0 +1,14 @@
+void main(){
+  final s=123;
+  const d=6.6;
+  var list=[1,2,3,4,5,6,7,8];
+  var map={'id':1,'name':'rakumadji'};
+  double n=7.7;
+  dynamic c='nulll';
+  print(s); 
+  print(d); 
+  print(list); 
+  print(map); 
+  print(n); 
+  print(c); 
+}
